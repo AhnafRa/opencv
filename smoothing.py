6d36a507ffefecf4d378d98a3f7d7976645692ec
Ahnaf Rashid <ahnaf.r0509@gmail.com>
2022-07-01 01:43:19 -0400

    masking push

---
+++ smoothing.py
@@ -0,0 +1,24 @@
+from re import I
+import cv2 as cv
+from cv2 import imshow
+
+img = cv.imread('Photos /pic.jpg')
+cv.imshow('Pic',img)
+
+#Average method for blurring
+avg = cv.blur(img, (3,3))
+cv.imshow('Average', avg)
+
+#Gaussian Blur method
+gaus = cv.GaussianBlur(img, (7,7), 0)
+cv.imshow('Gaussian', gaus)
+
+#Median Blur method
+med = cv.medianBlur(img,3)
+cv.imshow('Median', med)
+
+#Bilateral Blurring
+bilat = cv.bilateralFilter(img, 10, 35, 25)
+cv.imshow('Bilateral', bilat)
+
+cv.waitKey(0)
